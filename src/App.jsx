6a1b2629ec9941640543483165728a8d4652a466
Lee <elcheveremochilero@gmail.com>
2022-12-07 16:03:04 -0600

    add new ui and features

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import { ImBin, ImCheckmark, ImCheckmark2 } from "react-icons/im";
 
-function App() {
+const App = () => {
+
   const [input, setInput] = useState('')
   const [todos, setTodos] = useState([])
 
   const clearInput = () => {
     const target = document.getElementById('input')
-    target.value = '';
+    target.value = ''
+    setInput('')
   }
 
   const handleAdd = () => {
     if(input.trim() !== '' && input.length > 0){
       setTodos([...todos,{todo:input, id:todos.id, complete: false}])
     }
-    setInput('')
     clearInput()
-    
   }
   
   const handleClear = () => {
-    setInput('')
     setTodos([])
     clearInput()
   }
@@ -42,27 +42,41 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-br from-slate-200 via-cyan-200 to-amber-200">
-      <div className='w-full md:w-1/2 mx-10 h-3/4 bg-slate-200 bg-opacity-30 backdrop-blur-2xl ring-1 rounded-lg shadow-2xl break-words text-cyan-900'>
+    <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-br from-cyan-100 via-cyan-300 to-amber-200 outline-none font-serif">
+      <div className='w-full md:w-1/2 mx-10 h-3/4 bg-slate-200 bg-opacity-10 backdrop-blur-2xl ring-1 ring-cyan-200 rounded-lg shadow-xl break-words text-cyan-900'>
 
         <div className='flex flex-col justify-around'>
-          <input type="text" id="input" onChange={e => setInput(e.target.value)} className="w-full h-10 rounded-lg px-4"/>
-          <div className='flex justify-evenly text-lg'>
-            <button onClick={() => handleAdd()} className="bg-cyan-400 bg-opacity-40 drop-shadow-lg box-border w-1/2 h-10 m-4 rounded">Add Todo</button> 
-            <button onClick={() => handleClear()} className="bg-rose-400 bg-opacity-40 drop-shadow-lg box-border w-1/2 h-10 m-4 rounded">Clear All</button> 
+          <input type="text" id="input" onChange={e => setInput(e.target.value)} className="w-full h-10 shadow-inner rounded-lg px-4 outline-none caret-cyan-400 font-sans"/>
+          <div className='flex justify-evenly text-lg font-sans'>
+            <button onClick={() => handleAdd()} className="bg-cyan-400 bg-opacity-60 drop-shadow-lg box-border w-1/2 h-10 m-4 rounded outline-none brightness-100 hover:brightness-125 transition">Add Todo</button> 
+            <button onClick={() => handleClear()} className="bg-rose-500 bg-opacity-60 drop-shadow-lg box-border w-1/2 h-10 m-4 rounded outline-none brightness-100 hover:brightness-125 transition text-rose-50">Clear All</button> 
           </div>
         </div>
         
-        <div id='todo-box' className='overflow-y-auto h-3/4'>
-            {todos.map((todo,index) => {
-              index +=1;
-              todo.id = index
-
+        <div id='todo-box' className='overflow-y-auto h-3/4 flex flex-col items-center snap-y'>
+            {todos.map((todo) => {
+              todo.id = crypto.randomUUID()
+              const style = {
+                item:{
+                  pending:'flex gap-6 justify-between items-center w-5/6 px-10 py-4 my-4 bg-opacity-20 rounded shadow-md snap-start bg-cyan-500',
+                  completed:'flex gap-6 justify-between items-center w-5/6 px-10 py-4 my-4 bg-opacity-20 rounded shadow-md snap-start bg-emerald-200'
+                },
+                text:{
+                  pending:'text-xl w-2/3 font-serif tracking-wide',
+                  completed:'text-xl w-2/3 font-serif tracking-wide text-emerald-600 '
+                }
+              }
               return(
-                <div key={index} className='flex gap-6 justify-between items-center w-full px-10 py-6'>
-                  <span>{index}.</span> 
-                  <span className='cursor-pointer text-xl' onClick={() => handleComplete(todo.id)} style={{textDecoration : todo.complete && 'line-through'}}>{todo.todo}</span>
-                  <button onClick={() => handleDelete(todo.id)} className="px-6 py-4 bg-cyan-100 ring-1 rounded bg-opacity-40 shadow-md">DELETE</button>
+                <div key={todo.id} className={todo.complete ? style.item.completed : style.item.pending}>
+                  <span className={todo.complete ? style.text.completed : style.text.pending}>{todo.todo}</span>
+                  <span className='flex gap-4 text-2xl'>
+                    <button className='outline-none' onClick={() => handleComplete(todo.id)}>
+                      {todo.complete? <ImCheckmark className='text-emerald-600'/> : <ImCheckmark2 />}
+                    </button>
+                    <button className='outline-none' onClick={() => handleDelete(todo.id)}>
+                      <ImBin className='hover:text-rose-500 transition'/>
+                    </button>
+                  </span>
                 </div>
               )
             })}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Roboto', 'ui-sans-serif', 'system-ui'],
+      'serif': ['Roboto Serif', 'ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular']
+    },
     extend: {},
   },
   plugins: [],
